560e0ab8e64e9fe027fff10ac8193ee528c1cd5d
CHAMOK <chamok1995>
2021-09-07 23:03:27 +0600

    modified

--- app.js
+++ app.js
@@ -68,6 +68,10 @@ const createSlider = () => {
   // hide image aria
   imagesArea.style.display = 'none';
   const duration = document.getElementById('duration').value || 1000;
+  if (duration<0) {
+    alert("Please purt you Posetive Number")
+    return;
+  }
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
